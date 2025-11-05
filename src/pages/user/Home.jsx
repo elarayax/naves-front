366f77759be4699b43bf46f0fd1a910f20b761b8
elarayax <37583319+elarayax@users.noreply.github.com>
2025-11-05 16:14:55 -0300

    comenzado api

---
+++ src/pages/user/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import NavesService from '../../services/NavesService.jsx';
+
+const NavesList = () => {
+    
+    const [naves, setNaves] = useState([]);
+    
+    useEffect(() => {
+        fetchNaves();
+    }, []);
+
+    const fetchNaves = () => {
+        NavesService.getAllNaves().then(response => {
+            setNaves(response.data);
+        }).catch(error => {
+            console.log('Error fetching books:', error);
+        });
+    };
+    return (
+        <div>
+            <h2>Naves List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nombre nave</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {naves.map(nave => (
+                        <tr key={nave.id}>
+                            <td>{nave.nombre}</td>    
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>);
+    };
+export default NavesList;
---
+++ src/services/NavesService.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://naves-back.onrender.com/api/naves';
+
+class NavesService {
+
+    getAllNaves() {
+        return axios.get(BASE_URL);
+    }
+
+    getNaveById(id) {
+        return axios.get(`${BASE_URL}/${id}`);
+    }
+
+    createNave(nave) {
+        return axios.post(BASE_URL, nave);
+    }
+
+    updateNave(id, nave) {
+        return axios.put(`${BASE_URL}/${id}`, nave);
+    }
+
+    deleteNave(id) {
+        return axios.delete(`${BASE_URL}/${id}`);
+    }
+}
+export default new NavesService();
